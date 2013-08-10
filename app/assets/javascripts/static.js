c1fa44fb3501e76b985583e30f196504cc722d11
Michael Dawson <m.dawson@y7mail.com>
2013-08-10 19:25:39 +0930

    Major work restyling, adding images, some placeholder content

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,26 +1,95 @@
 jQuery(window).load(function(){
-    jQuery('.portfolioContainer').isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
-    });
- 
-    jQuery('.portfolioFilter a').click(function(){
-        jQuery('.portfolioFilter .current').removeClass('current');
-        jQuery(this).addClass('current');
- 
-        var selector = jQuery(this).attr('data-filter');
-        jQuery('.portfolioContainer').isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
+
+    $(function(){
+    
+        var $container = $('#container');
+    
+        // add randomish size classes
+        $container.find('.element').each(function(){
+            var $this = $(this),
+                number = parseInt( $this.find('.number').text(), 10 );
+            if ( number % 7 % 2 === 1 ) {
+              $this.addClass('width2');
             }
-         });
-         return false;
-    }); 
+            if ( number % 3 === 0 ) {
+              $this.addClass('height2');
+            }
+        });
+    
+        $container.isotope({
+          itemSelector : '.element',
+          masonry : {
+            columnWidth : 120
+          },
+          masonryHorizontal : {
+            rowHeight: 120
+          },
+          cellsByRow : {
+            columnWidth : 240,
+            rowHeight : 240
+          },
+          cellsByColumn : {
+            columnWidth : 240,
+            rowHeight : 240
+          },
+          getSortData : {
+            symbol : function( $elem ) {
+              return $elem.attr('data-symbol');
+            },
+            category : function( $elem ) {
+              return $elem.attr('data-category');
+            },
+            number : function( $elem ) {
+              return parseInt( $elem.find('.number').text(), 10 );
+            },
+            weight : function( $elem ) {
+              return parseFloat( $elem.find('.weight').text().replace( /[\(\)]/g, '') );
+            },
+            name : function ( $elem ) {
+              return $elem.find('.name').text();
+            }
+          }
+        });
+
+        
+        var $optionSets = $('#options .option-set'),
+          $optionLinks = $optionSets.find('a');
+
+        $optionLinks.click(function(){
+        var $this = $(this);
+        // don't proceed if already selected
+        if ( $this.hasClass('selected') ) {
+          return false;
+        }
+        var $optionSet = $this.parents('.option-set');
+        $optionSet.find('.selected').removeClass('selected');
+        $this.addClass('selected');
+
+        // make option object dynamically, i.e. { filter: '.my-filter-class' }
+        var options = {},
+            key = $optionSet.attr('data-option-key'),
+            value = $this.attr('data-option-value');
+        // parse 'false' as false boolean
+        value = value === 'false' ? false : value;
+        options[ key ] = value;
+        if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
+          // changes in layout modes need extra logic
+          changeLayoutMode( $this, options )
+        } else {
+          // otherwise, apply new options
+          $container.isotope( options );
+        }
+
+        return false;
+        });
+
+
+        // change size of clicked element
+        $container.delegate( '.element', 'click', function(){
+            $(this).toggleClass('large');
+            $(this).find(".large_show").toggleClass('hidden')
+            
+            $container.isotope('reLayout');
+        });
+    });
 });
