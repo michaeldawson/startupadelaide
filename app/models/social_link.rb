c3a352cb3d25beca3aed8ab774cb44a773add3c8
Michael Dawson <m.dawson@y7mail.com>
2013-09-10 12:10:02 +0930

    Added auto correction of links, iso link styling

--- app/models/social_link.rb
+++ app/models/social_link.rb
@@ -2,4 +2,17 @@ class SocialLink < ActiveRecord::Base
   attr_accessible :link_url, :node_id, :social_key, :node
 
   belongs_to :node
+
+  before_save :refactor_www
+
+
+  def refactor_www
+  	#take www.google.com and convert it to http://www.google.com
+
+  	url = self.link_url
+
+  	if url.match(/www/) && !(url.match(/http:\/\/www/))
+  		self.link_url = url.gsub(/www/, "http://www")
+  	end
+  end
 end
--- spec/models/social_link_spec.rb
+++ spec/models/social_link_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe SocialLink do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "replaces www with http format" do
+		@sl = FactoryGirl.create(:social_link, link_url: "www.google.com")
+		@sl.link_url.should == "http://www.google.com"
+	end
+
+	it "doesn't replace http://www" do
+		@sl = FactoryGirl.create(:social_link, link_url: "http://www.google.com")
+		@sl.link_url.should == "http://www.google.com"
+	end
 end
